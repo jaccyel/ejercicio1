fdc8e8d6af2a71532f6e58e3a1069a1b588dfc48
jaciel sanchez <jaciel8686@hotmail.com>
2016-11-15 08:18:03 -0700

    v1-genere tabla numerica con javascrip

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-// JavaScript Document
+// JavaScript Documentfunction mostrarNumero(){
+function mostrarNumeros(){
+    for (var i=0; i<=10; i++){
+		document.getElementById("tablaNumeros").innerHTML +="<tr><td>"+i+"</td></tr>";
+	}
+}
